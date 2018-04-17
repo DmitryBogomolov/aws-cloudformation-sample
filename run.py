27c1f27c84fb0bfd6b01b9157a2098145a01bb40
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2018-04-17 09:24:07 +0300

    Reorganize entry script

--- run.py
+++ run.py
@@ -6,38 +6,44 @@ import importlib
 import inspect
 import functools
 
-parser = argparse.ArgumentParser()
-subparsers = parser.add_subparsers(dest='command')
-
 COMMANDS = ('process', 'pack', 'deploy', 'delete', 'invoke', 'logs')
 
 def is_entry_function(name, obj):
     return inspect.isfunction(obj) and obj.__name__ == name
 
-functions = {}
+def setup_subparsers(subparsers):
+    functions = {}
+    for command in COMMANDS:
+        mod = importlib.import_module(command)
+        members = inspect.getmembers(mod, functools.partial(is_entry_function, command))
+        if len(members) == 0:
+            continue
+        func = members[0][1]
+        functions[command] = func
+        signature = inspect.signature(func)
+        subparser = subparsers.add_parser(command)
+        for parameter in signature.parameters.values():
+            args = { 'required': True }
+            if parameter.default is not signature.empty:
+                args['required'] = False
+            subparser.add_argument('--' + parameter.name, **args)
+    return functions
 
-for command in COMMANDS:
-    mod = importlib.import_module(command)
-    members = inspect.getmembers(mod, functools.partial(is_entry_function, command))
-    if len(members) == 0:
-        continue
-    func = members[0][1]
-    functions[command] = func
-    signature = inspect.signature(func)
-    subparser = subparsers.add_parser(command)
-    for parameter in signature.parameters.values():
-        args = { 'required': True }
-        if parameter.default is not signature.empty:
-            args['required'] = False
-        subparser.add_argument('--' + parameter.name, **args)
+def main():
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(dest='command')
+    functions = setup_subparsers(subparsers)
 
-ret = parser.parse_args()
-if not ret.command:
-    parser.print_help()
-    sys.exit(0)
+    args = vars(parser.parse_args())
+    command = args.pop('command')
+    if not command:
+        parser.print_help()
+        return 0
 
-func = functions[ret.command]
-args = vars(ret)
-args.pop('command')
-res = func(**args)
-print(res)
+    func = functions[command]
+    res = func(**args)
+    print(res)
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
