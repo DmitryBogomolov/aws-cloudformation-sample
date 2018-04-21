6df794be783fa39c830f3f9ead1e30b37d0c4a3e
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2018-04-21 16:51:11 +0300

    Allow to remove sources together with stack removing

--- remove.py
+++ remove.py
@@ -1,8 +1,11 @@
 import boto3
 import helper
+from remove_sources import remove_sources as call_remove_sources
 
 cf = boto3.client('cloudformation')
 
-def remove():
+def remove(remove_sources=False):
     template = helper.load_template()
     cf.delete_stack(StackName=template['Name'])
+    if remove_sources:
+        call_remove_sources()
