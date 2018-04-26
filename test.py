37b96b26c7184569db01697ef5ce412bde49e27f
Dmitry Bogomolov <Dmitry.Bogomolov.mail@gmail.com>
2018-04-27 00:12:08 +0300

    Rename name to project (#13)
    * Rename 'Name' to 'Project'
    
    * Add some more tests

--- test.py
+++ test.py
@@ -5,6 +5,10 @@ import os
 import helper
 
 class TestHelper(unittest.TestCase):
+    def test_get_processed_template_path(self):
+        self.assertEqual(helper.get_processed_template_path(),
+            os.path.realpath('.package/template.yaml'))
+
     def test_get_archive_name(self):
         cases = [
             ('test.t1', 'test_t1.zip'),
@@ -23,6 +27,14 @@ class TestHelper(unittest.TestCase):
 
     def test_get_archive_path(self):
         self.assertEqual(helper.get_archive_path('test.zip'), os.path.realpath('.package/test.zip'))
+
+    def test_get_function_name(self):
+        template = { 'Project': 'proj1' }
+        self.assertEqual(helper.get_function_name(template, 'func1'), 'proj1-func1')
+
+    def test_get_s3_key(self):
+        template = { 'Project': 'proj1' }
+        self.assertEqual(helper.get_s3_key(template, 'obj1'), 'proj1/obj1')
 
 if __name__ == '__main__':
     unittest.main()
