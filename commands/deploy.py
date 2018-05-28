00743c77c28921f6dec18e3d3a3dcdcfd6c6c97c
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2018-05-28 16:26:54 +0300

    Use waiters in 'deploy' and 'remove'

--- commands/deploy.py
+++ commands/deploy.py
@@ -1,19 +1,79 @@
-import subprocess
+import boto3
 from utils import helper
 from utils.logger import log
 
-# There is no *boto3.client('cloudformation').deploy* function - have to use *subprocess*.
-def invoke_deploy(template):
-    args = [
-        'aws', 'cloudformation', 'deploy',
-        '--stack-name', template['Project'],
-        '--capabilities', 'CAPABILITY_IAM',
-        '--template-file', helper.get_processed_template_path()
-    ]
-    with subprocess.Popen(args) as proc:
-        proc.communicate()
+cf = boto3.client('cloudformation')
+
+TEMPLATE_BODY = '''
+AWSTemplateFormatVersion: 2010-09-09
+
+Resources:
+  StackLogGroup:
+    Type: AWS::Logs::LogGroup
+    Properties:
+      LogGroupName: /aws/stack/{project}
+'''
+
+def get_template_body():
+    with open(helper.get_processed_template_path()) as f:
+        return f.read()
+
+def create_stack(stack_name):
+    try:
+        cf.create_stack(
+            StackName=stack_name,
+            TemplateBody=TEMPLATE_BODY.format(project=stack_name)
+        )
+        log('creating stack')
+        waiter = cf.get_waiter('stack_create_complete')
+        waiter.wait(
+            StackName=stack_name,
+            WaiterConfig={ 'Delay': 5 }
+        )
+        log('stack is created')
+    except cf.exceptions.AlreadyExistsException:
+        log('stack already exists')
+
+def update_stack(stack_name, template_body):
+    log('creating change set')
+    ret = cf.create_change_set(
+        StackName=stack_name,
+        Capabilities=('CAPABILITY_IAM',),
+        TemplateBody=template_body,
+        ChangeSetName='change-set'
+    )
+    change_id = ret['Id']
+    try:
+        waiter = cf.get_waiter('change_set_create_complete')
+        waiter.wait(
+            StackName=stack_name,
+            ChangeSetName=change_id,
+            WaiterConfig={ 'Delay': 5 }
+        )
+    except boto3.exceptions.botocore.exceptions.WaiterError as err:
+        if len(err.last_response['Changes']) == 0:
+            log('no changes')
+            return
+        raise err
+    log('change set is created')
+    log('updating stack')
+    cf.execute_change_set(
+        StackName=stack_name,
+        ChangeSetName=change_id
+    )
+    waiter = cf.get_waiter('stack_update_complete')
+    waiter.wait(
+        StackName=stack_name,
+        WaiterConfig={ 'Delay': 15 }
+    )
+    log('stack is updated')
 
 def run():
     log('Deploying stack')
     template = helper.load_template()
-    invoke_deploy(template)
+    stack_name = template['Project']
+    template_body = get_template_body()
+    cf.validate_template(TemplateBody=template_body)
+    create_stack(stack_name)
+    update_stack(stack_name, template_body)
+    log('Done')
--- commands/remove.py
+++ commands/remove.py
@@ -5,9 +5,18 @@ from .remove_sources import run as call_remove_sources
 
 cf = boto3.client('cloudformation')
 
+def delete_stack(stack_name):
+    cf.delete_stack(StackName=stack_name)
+    waiter = cf.get_waiter('stack_delete_complete')
+    waiter.wait(
+        StackName=stack_name,
+        WaiterConfig={ 'Delay': 5 }
+    )
+
 def run(remove_sources=False):
     log('Removing stack')
     template = helper.load_template()
-    cf.delete_stack(StackName=template['Project'])
+    delete_stack(template['Project'])
+    log('Done')
     if remove_sources:
         call_remove_sources()
