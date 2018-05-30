8f2e05670e85c222aca9fa99a49ef3a6e5e1e365
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2018-05-30 13:51:16 +0300

    Refactor template generation

--- commands/process.py
+++ commands/process.py
@@ -1,100 +1,12 @@
-import yaml
 from utils import helper
-from utils.pattern import pattern, Function, LambdaRole
+from utils.pattern import pattern
 from utils.logger import log
 from utils.yaml import save
-
-ROOT_TEMPLATE = '''
-AWSTemplateFormatVersion: 2010-09-09
-Transform: AWS::Serverless-2016-10-31
-Description: <Description>
-Resources: {}
-'''
-
-FUNCTION_TEMPLATE = '''
-Type: AWS::Serverless::Function
-Properties:
-  Environment:
-    Variables: {}
-  Tags: {}
-'''
-
-ROLE_TEMPLATE = '''
-Type: AWS::IAM::Role
-Properties:
-  AssumeRolePolicyDocument:
-    Version: 2012-10-17
-    Statement:
-      - Effect: Allow
-        Principal:
-          Service: lambda.amazonaws.com
-        Action: sts:AssumeRole
-  Path: /
-  ManagedPolicyArns:
-    - arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole
-  Policies: []
-'''
-
-POLICY_TEMPLATE = '''
-PolicyName: <PolicyName>
-PolicyDocument:
-  Version: 2012-10-17
-  Statement: []
-'''
-
-def create_template(pattern):
-    template = yaml.load(ROOT_TEMPLATE)
-    template['Description'] = pattern.description
-    template['Resources'].update(pattern.Resources)
-    return template
-
-def create_function(function):
-    resource = yaml.load(FUNCTION_TEMPLATE)
-    properties = resource['Properties']
-    properties.update(function.Properties)
-    properties.update(
-        FunctionName=function.full_name,
-        Description=function.description,
-        Runtime=function.runtime,
-        Timeout=function.timeout,
-        Tags=function.tags,
-        Handler=function.handler,
-        Role=function.role,
-        CodeUri='s3://{}/{}/{}'.format(
-            pattern.bucket, pattern.project, helper.get_archive_name(function.code_uri))
-    )
-    if function.environment:
-        properties['Environment']['Variables'].update(function.environment)
-    return resource
-
-def create_role(role):
-    resource = yaml.load(ROLE_TEMPLATE)
-    properties = resource['Properties']
-    properties.update(role.Properties)
-    policy_resouces = []
-    for policy in role.policies:
-        policy_resouce = yaml.load(POLICY_TEMPLATE)
-        policy_resouce['PolicyName'] = policy.name
-        policy_resouce['PolicyDocument']['Statement'].extend(policy.statement)
-        policy_resouces.append(policy_resouce)
-    properties['Policies'].extend(policy_resouces)
-    return resource
-
-def create_resources(template, resources):
-    for resource in resources:
-        if isinstance(resource, Function):
-            template[resource.name] = create_function(resource)
-        if isinstance(resource, LambdaRole):
-            template[resource.name] = create_role(resource)
-
-def save_template(template):
-    file_path = helper.get_processed_template_path()
-    save(file_path, template);
-    log('Saved to {}', file_path)
 
 def run():
     log('Processing {}', helper.get_pattern_path())
     helper.ensure_folder()
-    template = create_template(pattern)
-    create_resources(template['Resources'], pattern.resources)
-    save_template(template)
+    template = pattern.dump()
+    file_path = helper.get_processed_template_path()
+    save(file_path, template);
+    log('Saved to {}', file_path)
--- utils/pattern.py
+++ utils/pattern.py
@@ -1,10 +1,27 @@
+import yaml
 from . import helper
 from .yaml import load
 
 def take_dict(source, field):
     return source.get(field) or {}
 
-class Root(object):
+
+class Base(object):
+    def dump(self):
+        resource = yaml.load(self.TEMPLATE)
+        self._dump(resource)
+        return resource
+
+
+class Root(Base):
+    TEMPLATE = \
+'''
+AWSTemplateFormatVersion: 2010-09-09
+Transform: AWS::Serverless-2016-10-31
+Description: <Description>
+Resources: {}
+'''
+
     def __init__(self, source):
         self.Resources = take_dict(source, 'Resources')
         self.project = source['project']
@@ -18,7 +35,23 @@ class Root(object):
         self.functions = []
         self.roles = []
 
-class Function(object):
+    def _dump(self, resource):
+        resource['Description'] = self.description
+        resource['Resources'].update(self.Resources)
+        resources = { obj.name: obj.dump() for obj in self.resources }
+        resource['Resources'].update(resources)
+
+
+class Function(Base):
+    TEMPLATE = \
+'''
+Type: AWS::Serverless::Function
+Properties:
+  Environment:
+    Variables: {}
+  Tags: {}
+'''
+
     def __init__(self, name, source, root):
         self.root = root
         self.name = name
@@ -34,18 +67,72 @@ class Function(object):
         self.role = source.get('role')
         self.environment = take_dict(source, 'environment')
 
-class Policy(object):
+    def _dump(self, resource):
+        properties = resource['Properties']
+        properties.update(self.Properties)
+        properties.update(
+            FunctionName=self.full_name,
+            Description=self.description,
+            Runtime=self.runtime,
+            Timeout=self.timeout,
+            Tags=self.tags,
+            Handler=self.handler,
+            Role=self.role,
+            CodeUri='s3://{}/{}/{}'.format(
+                self.root.bucket, self.root.project, helper.get_archive_name(self.code_uri))
+        )
+        properties['Environment']['Variables'].update(self.environment)
+
+
+class Policy(Base):
+    TEMPLATE = \
+'''
+PolicyName: <PolicyName>
+PolicyDocument:
+  Version: 2012-10-17
+  Statement: []
+'''
+
     def __init__(self, name, statement):
         self.name = name
         self.statement = statement
 
-class LambdaRole(object):
+    def _dump(self, resource):
+        resource['PolicyName'] = self.name
+        resource['PolicyDocument']['Statement'].extend(self.statement)
+
+
+class LambdaRole(Base):
+    TEMPLATE = \
+'''
+Type: AWS::IAM::Role
+Properties:
+  AssumeRolePolicyDocument:
+    Version: 2012-10-17
+    Statement:
+      - Effect: Allow
+        Principal:
+          Service: lambda.amazonaws.com
+        Action: sts:AssumeRole
+  Path: /
+  ManagedPolicyArns:
+    - arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole
+  Policies: []
+'''
+
     def __init__(self, name, source, root):
         self.root = root
         self.name = name
         self.Properties = take_dict(source, 'Properties')
 
         self.policies = [Policy(**obj) for obj in source['policies']]
+
+    def _dump(self, resource):
+        properties = resource['Properties']
+        properties.update(self.Properties)
+        policies = [policy.dump() for policy in self.policies]
+        properties['Policies'].extend(policies)
+
 
 def check_required_fields(source):
     absent = []
