9aa1854be739e17c15a97ed6d232f1381884d827
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2018-06-01 11:21:20 +0300

    Handle waiter error in remove

--- commands/deploy.py
+++ commands/deploy.py
@@ -33,6 +33,9 @@ def create_stack(stack_name):
         log('stack is created')
     except cf.exceptions.AlreadyExistsException:
         log('stack already exists')
+    except WaiterError as err:
+        log('stack is not created')
+        raise RuntimeError(err.last_response)
 
 def update_stack(stack_name, template_body):
     log('creating change set')
--- commands/remove.py
+++ commands/remove.py
@@ -1,7 +1,7 @@
 from utils import helper
 from utils.client import client
 from utils.pattern import pattern
-from utils.cf_waiter import wait
+from utils.cf_waiter import wait, WaiterError
 from utils.logger import log
 from .remove_sources import run as call_remove_sources
 
@@ -9,7 +9,10 @@ cf = client('cloudformation')
 
 def delete_stack(stack_name):
     cf.delete_stack(StackName=stack_name)
-    wait('stack_delete_complete', stack_name, 5)
+    try:
+        wait('stack_delete_complete', stack_name, 5)
+    except WaiterError as err:
+        raise RuntimeError(err.last_response)
 
 def run(remove_sources=False):
     log('Removing stack')
