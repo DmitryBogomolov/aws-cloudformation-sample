34567ac45a851c95562cc8834fea94498063ba25
Dmitry Bogomolov <Dmitry.Bogomolov.mail@gmail.com>
2018-06-03 15:22:11 +0300

    Move resources classes into separate package (#30)

---
+++ pattern/base_resource.py
@@ -0,0 +1,30 @@
+import yaml
+from .base import Base
+
+class BaseResource(Base):
+    def __init__(self, name, source, root):
+        super().__init__(source)
+        self.name = name
+        self.root = root
+
+    def dump(self, parent_template):
+        template = yaml.load(self.TEMPLATE)
+        self._dump(template, parent_template)
+        parent_template['Resources'][self.name] = template
+
+    def _dump(self, template, parent_template):
+        properties = template['Properties']
+        properties.update(self.get('Properties', {}))
+
+        # The following allows to avoid declaring "DependsOn: []" in every TEMPLATE field.
+        depends_on = template.get('DependsOn')
+        if not depends_on:
+            depends_on = []
+            template['DependsOn'] = depends_on
+        depends_on.extend(self.get('DependsOn', []))
+        depends_on.extend(self.get('depends_on', []))
+        self._dump_properties(properties)
+
+
+    def _dump_properties(self, properties):
+        raise Exception('Not implemented')
