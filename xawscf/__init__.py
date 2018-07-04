b1a39d98bf466f149a2c2191eebb607bb845acc9
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2018-07-04 10:28:31 +0300

    Remove pattern loading code duplication

--- xawscf/__init__.py
+++ xawscf/__init__.py
@@ -2,20 +2,15 @@
 Executes different commands.
 '''
 
-import sys
 import argparse
-import inspect
-
-from .commands import commands as actions
+from .commands import commands
 
 def setup_subparsers(subparsers):
-    for command, func in actions.items():
-        signature = inspect.signature(func)
-        subparser = subparsers.add_parser(command,
-            description=sys.modules[func.__module__].__doc__)
-        for parameter in signature.parameters.values():
+    for command in commands:
+        subparser = subparsers.add_parser(command.name, description=command.description)
+        for parameter in command.parameters:
             args = { 'required': True }
-            if parameter.default is not signature.empty:
+            if hasattr(parameter, 'default'):
                 args['required'] = False
                 if isinstance(parameter.default, bool):
                     args['action'] = 'store_false' if parameter.default else 'store_true'
@@ -27,9 +22,10 @@ def run():
     setup_subparsers(subparsers)
 
     args = vars(parser.parse_args())
-    command = args.pop('command')
-    if not command:
+    command_name = args.pop('command')
+    if not command_name:
         parser.print_help()
         return 0
 
-    return actions[command](**args)
+    execute = next(command.execute for command in commands if command.name == command_name)
+    return execute(**args)
