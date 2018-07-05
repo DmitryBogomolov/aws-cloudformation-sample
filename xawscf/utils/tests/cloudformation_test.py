b2a74315512fa2ba2c668863fdc158eedad52df7
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2018-07-05 16:19:26 +0300

    Fix pylint in 'utils' package

--- xawscf/utils/tests/cloudformation_test.py
+++ xawscf/utils/tests/cloudformation_test.py
@@ -1,5 +1,4 @@
 import unittest
-import boto3
 from .. import cloudformation
 
 StubCloudFormation = type('StubCloudFormation', (), {})
@@ -7,24 +6,26 @@ StubCloudFormation = type('StubCloudFormation', (), {})
 class TestCloudFormation(unittest.TestCase):
     def test_get_stack_info(self):
         data = {
-            'stack-1': { 'Stacks': [{ 'tag': 'test-stack-1' }] }
+            'stack-1': {'Stacks': [{'tag': 'test-stack-1'}]}
         }
+        # pylint: disable=invalid-name
         def describe_stacks(StackName):
             return data[StackName]
         stub = StubCloudFormation()
         stub.describe_stacks = describe_stacks
 
-        self.assertEqual(cloudformation.get_stack_info(stub, 'stack-1'), { 'tag': 'test-stack-1' })
+        self.assertEqual(cloudformation.get_stack_info(stub, 'stack-1'), {'tag': 'test-stack-1'})
 
     def test_get_sources_bucket(self):
         outputs = [
-            { 'OutputKey': 'key-1' },
-            { 'OutputKey': 'SourcesBucket', 'OutputValue': 'test-bucket-1' },
-            { 'OutputKey': 'key-2' }
+            {'OutputKey': 'key-1'},
+            {'OutputKey': 'SourcesBucket', 'OutputValue': 'test-bucket-1'},
+            {'OutputKey': 'key-2'}
         ]
         data = {
-            'stack-1': { 'Stacks': [{ 'Outputs': outputs }] }
+            'stack-1': {'Stacks': [{'Outputs': outputs}]}
         }
+        # pylint: disable=invalid-name
         def describe_stacks(StackName):
             return data[StackName]
         stub = StubCloudFormation()
--- xawscf/utils/tests/loader_test.py
+++ xawscf/utils/tests/loader_test.py
@@ -5,8 +5,8 @@ from .. import loader
 FILE_NAME = 'test.yaml'
 
 def create_file(content):
-    with open(FILE_NAME, 'w') as f:
-        f.write(content)
+    with open(FILE_NAME, 'w') as file_object:
+        file_object.write(content)
 
 class TestYaml(unittest.TestCase):
     def tearDown(self):
@@ -19,22 +19,22 @@ class TestYaml(unittest.TestCase):
 
     def check_save(self, obj, expected):
         loader.save(FILE_NAME, obj)
-        with open(FILE_NAME, 'r') as f:
-            content = f.read()
+        with open(FILE_NAME, 'r') as file_object:
+            content = file_object.read()
         self.assertEqual(content, expected)
 
     def test_load(self):
-        self.check_load('a: test', { 'a': 'test' })
+        self.check_load('a: test', {'a': 'test'})
 
     def test_load_intrinsic_scalar(self):
-        self.check_load('a: !Ref b', { 'a': loader.Custom('!Ref', 'b') })
+        self.check_load('a: !Ref b', {'a': loader.Custom('!Ref', 'b')})
 
     def test_load_intrinsic_list(self):
-        self.check_load('a: !Sub [1, 2]', { 'a': loader.Custom('!Sub', [1, 2]) })
+        self.check_load('a: !Sub [1, 2]', {'a': loader.Custom('!Sub', [1, 2])})
 
     def test_load_intrinsic_dict(self):
         self.check_load('a: { b: !GetAtt c.d, c: 1 }',
-            { 'a': { 'b': loader.Custom('!GetAtt', 'c.d'), 'c': 1 } })
+            {'a': {'b': loader.Custom('!GetAtt', 'c.d'), 'c': 1}})
 
     def test_load_intrinsic_complex(self):
         self.check_load('a: !Sub [test, { b: !Ref c, d: !GetAtt e }]', {
@@ -45,16 +45,16 @@ class TestYaml(unittest.TestCase):
         })
 
     def test_save(self):
-        self.check_save({ 'a': 'test' }, 'a: test\n')
+        self.check_save({'a': 'test'}, 'a: test\n')
 
     def test_save_intrinsic_scalar(self):
-        self.check_save({ 'a': loader.Custom('!Ref', 'b') }, "a: !Ref 'b'\n")
+        self.check_save({'a': loader.Custom('!Ref', 'b')}, "a: !Ref 'b'\n")
 
     def test_save_intrinsic_list(self):
-        self.check_save({ 'a': loader.Custom('!Sub', [1, 2]) }, "a: !Sub\n- 1\n- 2\n")
+        self.check_save({'a': loader.Custom('!Sub', [1, 2])}, "a: !Sub\n- 1\n- 2\n")
 
     def test_save_intrinsic_dict(self):
-        self.check_save({ 'a': { 'b': loader.Custom('!GetAtt', 'c.d'), 'c': 1 } },
+        self.check_save({'a': {'b': loader.Custom('!GetAtt', 'c.d'), 'c': 1}},
             "a:\n  b: !GetAtt 'c.d'\n  c: 1\n")
 
     def test_save_intrinsic_complex(self):
@@ -66,17 +66,19 @@ class TestYaml(unittest.TestCase):
         }, "a: !Sub\n- test\n- b: !Ref 'c'\n  d: !GetAtt 'e'\n")
 
     def test_load_with_included(self):
-        with open('file1.yaml', 'w') as f:
-            f.write('b: test')
+        with open('file1.yaml', 'w') as file_object:
+            file_object.write('b: test')
         try:
-            self.check_load('a: !include file1.yaml', { 'a': { 'b': 'test' } })
+            self.check_load('a: !include file1.yaml', {'a': {'b': 'test'}})
         finally:
             os.remove('file1.yaml')
 
     def test_load_with_included_not_exist(self):
+        # pylint: disable=no-member
         with self.assertRaisesRegex(Exception, 'No such file or directory'):
             self.check_load('a: !include file1.yaml', None)
 
     def test_load_with_included_out_of_dir(self):
+        # pylint: disable=no-member,anomalous-backslash-in-string
         with self.assertRaisesRegex(Exception, 'File path \*..\/file1.yaml\* is not valid'):
             self.check_load('a: !include ../file1.yaml', None)
